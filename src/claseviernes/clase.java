5ad113f334b3ec3adb2ecce03f5c4d946ad99a92
Usuario <Usuario@JeanD7.lan>
2023-05-02 14:25:17 -0500

    Ejercicio 1

---
+++ src/claseviernes/clase.java
@@ -0,0 +1,24 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package claseviernes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class clase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ int j=0;
+ for (int i= 0; i<10; i++) {
+     j=j+2;
+     System.out.println("Pasada: "+i);
+     System.out.println("Pasada por 2: "+j);
+ }
+ System.out.println("Realizado por Jean De la Cruz"); 
+}}
